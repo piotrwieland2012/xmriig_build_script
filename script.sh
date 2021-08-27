f4ceb7a9e96209ddea9f5dc38ff3b824972f88e5
Piotr Wieland <53531766+piotrwieland2012@users.noreply.github.com>
2021-08-27 21:04:11 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,16 @@
 sudo apt update
 sudo apt-get install git build-essential cmake automake libtool autoconf screen 
 git clone https://github.com/xmrig/xmrig.git
-mkdir xmrig/build && cd xmrig/scripts
-./build_deps.sh && cd ../build
+mkdir xmrig
+cd xmrig
+mkdir build
+cd build
+cd ..
+cd scripts
+./build_deps
+./randomx_boost.sh
+./enable_1gb_pages.sh
+./build_deps.sh 
+cd ../build
 cmake .. -DXMRIG_DEPS=scripts/deps
 make -j$(nproc)
